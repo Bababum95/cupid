7d07dac2c93eeef4271de092234a20792f52fdee
Andrei <116939459+Bababum95@users.noreply.github.com>
2024-11-05 17:39:12 +0700

    Enable daily log rotation, conditional directory creation, and console logging.

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,29 +1,42 @@
 import { createLogger, format, transports } from "winston";
-// import DailyRotateFile from "winston-daily-rotate-file";
+import DailyRotateFile from "winston-daily-rotate-file";
 import path from "path";
-// import fs from "fs";
+import fs from "fs";
 
 const logsDirectory = path.join(process.cwd(), "logs");
-console.log(logsDirectory);
-// if (!fs.existsSync(logsDirectory)) fs.mkdirSync(logsDirectory);
+
+if (!process.env.IS_VERCEL && !fs.existsSync(logsDirectory)) {
+  fs.mkdirSync(logsDirectory);
+}
+
+const loggerTransports = [];
+
+if (!process.env.IS_VERCEL) {
+  loggerTransports.push(
+    new DailyRotateFile({
+      filename: path.join(logsDirectory, "error-%DATE%.log"),
+      level: "warn",
+      datePattern: "YYYY-MM-DD",
+      maxSize: "20m",
+      maxFiles: "14d",
+    })
+  );
+  loggerTransports.push(
+    new DailyRotateFile({
+      filename: path.join(logsDirectory, "combined-%DATE%.log"),
+      datePattern: "YYYY-MM-DD",
+      maxSize: "20m",
+      maxFiles: "3d",
+    })
+  );
+}
+
+if (process.env.NODE_ENV === "development") {
+  loggerTransports.push(new transports.Console());
+}
 
 export const logger = createLogger({
   level: process.env.NODE_ENV === "production" ? "warn" : "debug",
   format: format.combine(format.timestamp(), format.simple()),
-  transports: [
-    new transports.Console(),
-    //   new DailyRotateFile({
-    //     filename: path.join(logsDirectory, "error-%DATE%.log"),
-    //     level: "warn",
-    //     datePattern: "YYYY-MM-DD",
-    //     maxSize: "20m",
-    //     maxFiles: "14d",
-    //   }),
-    // new DailyRotateFile({
-    //   filename: path.join(logsDirectory, "combined-%DATE%.log"),
-    //   datePattern: "YYYY-MM-DD",
-    //   maxSize: "20m",
-    //   maxFiles: "3d",
-    // }),
-  ],
+  transports: loggerTransports,
 });
