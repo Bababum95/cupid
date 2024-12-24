5f23a2a26fd2ab53f590feb65b4d17a5d719c9c4
Andrei <116939459+Bababum95@users.noreply.github.com>
2024-12-24 11:30:35 +0700

    test 1.15

--- src/app/[locale]/cart/ChekoutLink.tsx
+++ src/app/[locale]/cart/ChekoutLink.tsx
@@ -8,15 +8,9 @@ type Props = {
   url: string;
 };
 
-const CheckoutLink = forwardRef<HTMLAnchorElement, Props>(({ url }, ref) => {
-  return (
-    <div className={styles.container}>
-      <a id="checkout-link" className={styles.link} href={url} ref={ref}>
-        Checkout
-      </a>
-    </div>
-  );
-});
+const CheckoutLink = forwardRef<HTMLAnchorElement, Props>(({ url }, ref) => (
+  <a id="checkout-link" className={styles.link} href={url} ref={ref} />
+));
 
 CheckoutLink.displayName = "CheckoutLink";
 
