66c987c99ac8ca37cb0b7ba9bdf05fad407d5248
Andrei <116939459+Bababum95@users.noreply.github.com>
2024-12-24 11:11:21 +0700

    test 1.9

--- src/app/[locale]/cart/ChekoutLink.tsx
+++ src/app/[locale]/cart/ChekoutLink.tsx
@@ -2,12 +2,18 @@
 
 import { forwardRef } from "react";
 
+import styles from "./CheckoutLink.module.scss";
+
 type Props = {
   url: string;
 };
 
 const CheckoutLink = forwardRef<HTMLAnchorElement, Props>(({ url }, ref) => {
-  return <a id="checkout-link" href={url} ref={ref}></a>;
+  return (
+    <a id="checkout-link" className={styles.link} href={url} ref={ref}>
+      Checkout
+    </a>
+  );
 });
 
 CheckoutLink.displayName = "CheckoutLink";
