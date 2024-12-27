5f893c1dad211793f9df286286ae5dc772f887c8
Andrei <116939459+Bababum95@users.noreply.github.com>
2024-12-27 10:21:10 +0700

    state SubscribePopup DELAY

--- src/components/dynamic/SubscribePopup/config.ts
+++ src/components/dynamic/SubscribePopup/config.ts
@@ -1,6 +1,6 @@
 import type { Variants } from "motion/react";
 
-export const DELAY = 100;
+export const DELAY = 20000;
 
 export const VARIANTS: Variants = {
   hidden: { opacity: 0, visibility: "hidden", display: "none" },
